b6863c6dd82db0f1ea34ec5f7ba3771dd769194c
kent2ckymaduka@gmail.com <kent2ckymaduka@gmail.com>
2019-11-15 02:09:19 +0100

    chore(fetch):fetch geoCordinates of searched place
    Use fetch api to fetch geoCoordinates of the place that the user
    searched for. The getGeoCodingInfoOfSearchedPlace() function takes
    care of that.
    Here I also added eslint to the project to make my code more es6
    compliant as is required for this project
    
    [Start #3]

--- js/main.js
+++ js/main.js
@@ -1,209 +1,289 @@
-const APPLICATION_ID = localStorage.getItem("_appId");
-const APPLICATION_CODE = localStorage.getItem("_appCode");
+const APPLICATION_ID = localStorage.getItem('_appId');
+const APPLICATION_CODE = localStorage.getItem('_appCode');
+// const OPENWEATHERMAP_APPID = localStorage.getItem('_openWeatherMapAppId');
 
-$(document).ready(() => {
-  console.log("We are live!");
-});
-
-// this handles the navigation toggle functionality.
-$(".nav_handle-container").click(() => {
-  if ($(".toggled").is(":visible")) {
-    $(".toggled").hide(200);
-    return;
-  }
-  $(".toggled").show(200);
-});
-
-//#region auto suggestion
-let AUTOCOMPLETION_URL =
-  "https://autocomplete.geocoder.api.here.com/6.2/suggest.json";
-// click and keyboard events for autosuggestion.
-
-$(document).on("click", "#1", () => {
-  const suggestion = $("#1").text();
-  insertSuggestionToTextbox(suggestion);
-});
-
-$(document).on("click", "#2", () => {
-  const suggestion = $("#2").text();
-  insertSuggestionToTextbox(suggestion);
-});
-
-$(document).on("click", "#3", () => {
-  const suggestion = $("#3").text();
-  insertSuggestionToTextbox(suggestion);
-});
-
-$(document).on("click", "#4", () => {
-  const suggestion = $("#4").text();
-  insertSuggestionToTextbox(suggestion);
-});
-
-$(document).on("click", "#5", () => {
-  const suggestion = $("#5").text();
-  insertSuggestionToTextbox(suggestion);
-});
-
-$(document).on("keyup", "#1", e => {
-  if (e.which == 13 || e.which == 32) {
-    const suggestion = $("#1").text();
-    insertSuggestionToTextbox(suggestion);
-  }
-});
-
-$(document).on("keyup", "#2", e => {
-  if (e.which == 13 || e.which == 32) {
-    const suggestion = $("#2").text();
-    insertSuggestionToTextbox(suggestion);
-  }
-});
-
-$(document).on("keyup", "#3", e => {
-  if (e.which == 13 || e.which == 32) {
-    const suggestion = $("#3").text();
-    insertSuggestionToTextbox(suggestion);
-  }
-});
-
-$(document).on("keyup", "#4", e => {
-  if (e.which == 13 || e.which == 32) {
-    const suggestion = $("#4").text();
-    insertSuggestionToTextbox(suggestion);
-  }
-});
-
-$(document).on("keyup", "#5", e => {
-  if (e.which == 13 || e.which == 32) {
-    const suggestion = $("#5").text();
-    insertSuggestionToTextbox(suggestion);
-  }
-});
 
 function insertSuggestionToTextbox(suggestion) {
-  $(".suggestions").remove();
-  $("input").val(suggestion);
-  $("input").focus();
+  $('.suggestions').remove();
+  $('input').val(suggestion);
+  $('input').focus();
 }
 
 function createAndAppendAutoCompleteElements(suggestions) {
-  // early exit
+  // early exit function validation
   if (!suggestions) {
-    $(".suggestions").remove();
-    console.log("suggestions is: " + suggestions);
+    $('suggestions').remove();
     return;
   }
 
-  $(".suggestions").remove();
-  console.log(suggestions);
+  $('.suggestions').remove();
   let id = 0;
-  let suggestion = suggestions.map(suggestions => {
-    id++;
-    return `<div class="suggestions" id=${id} tabindex="0"> ${suggestions.label} </div>`;
+  const result = suggestions.map((suggestion) => {
+    id += 1;
+    return `<div class="suggestions" id=${id} countrycode=${suggestion.countryCode} tabindex="0"> ${suggestion.label} </div>`;
   });
-  $("form").append(suggestion);
+  $('form').append(result);
 }
 
-$(".autocomplete").keyup(e => {
-  // Exit on esc keypress event
-  if (e.which == 27) {
-    $(".suggestions").remove();
+
+// this handles the navigation toggle functionality.
+$('.nav_handle-container').click(() => {
+  if ($('.toggled').is(':visible')) {
+    $('.toggled').hide(200);
     return;
   }
-
-  let searchValue = $(".autocomplete").val(); //grab the value in the textbox here
-
-  let params =
-    "?" +
-    "query=" +
-    encodeURIComponent(searchValue) + // The search text which is the basis of the query
-    "&beginHighlight=" +
-    encodeURIComponent("<mark>") + //  Mark the beginning of the match in a token.
-    "&endHighlight=" +
-    encodeURIComponent("</mark>") + //  Mark the end of the match in a token.
-    "&maxresults=5" + // The upper limit the for number of suggestions to be included
-    // in the response.  Default is set to 5.
-    "&app_id=" +
-    APPLICATION_ID +
-    "&app_code=" +
-    APPLICATION_CODE;
-
-  fetch(AUTOCOMPLETION_URL + params)
-    .then(response => response.json()) // convert response to json object
-    .then(result => {
-      const { suggestions } = result;
-      createAndAppendAutoCompleteElements(suggestions);
-    })
-    .catch(error => {
-      console.log("error fetching autocompletion data.");
-    });
+  $('.toggled').show(200);
 });
 
-//#endregion
+// #region auto suggestion
+const AUTOCOMPLETION_URL = 'https://autocomplete.geocoder.api.here.com/6.2/suggest.json';
+// click and keyboard events for autosuggestion.
 
-//#region map
+$(document).on('click', '#1', () => {
+  const suggestion = $('#1').text();
+  insertSuggestionToTextbox(suggestion);
+});
+
+$(document).on('click', '#2', () => {
+  const suggestion = $('#2').text();
+  insertSuggestionToTextbox(suggestion);
+});
+
+$(document).on('click', '#3', () => {
+  const suggestion = $('#3').text();
+  insertSuggestionToTextbox(suggestion);
+});
+
+$(document).on('click', '#4', () => {
+  const suggestion = $('#4').text();
+  insertSuggestionToTextbox(suggestion);
+});
+
+$(document).on('click', '#5', () => {
+  const suggestion = $('#5').text();
+  insertSuggestionToTextbox(suggestion);
+});
+
+$(document).on('keyup', '#1', (e) => {
+  if (e.which === 13 || e.which === 32) {
+    const suggestion = $('#1').text();
+    insertSuggestionToTextbox(suggestion);
+  }
+});
+
+$(document).on('keyup', '#2', (e) => {
+  if (e.which === 13 || e.which === 32) {
+    const suggestion = $('#2').text();
+    insertSuggestionToTextbox(suggestion);
+  }
+});
+
+$(document).on('keyup', '#3', (e) => {
+  if (e.which === 13 || e.which === 32) {
+    const suggestion = $('#3').text();
+    insertSuggestionToTextbox(suggestion);
+  }
+});
+
+$(document).on('keyup', '#4', (e) => {
+  if (e.which === 13 || e.which === 32) {
+    const suggestion = $('#4').text();
+    insertSuggestionToTextbox(suggestion);
+  }
+});
+
+$(document).on('keyup', '#5', (e) => {
+  if (e.which === 13 || e.which === 32) {
+    const suggestion = $('#5').text();
+    insertSuggestionToTextbox(suggestion);
+  }
+});
+
+$('.autocomplete').keyup((e) => {
+  // Exit on esc keypress event
+  if (e.which === 27) {
+    $('.suggestions').remove();
+    return;
+  }
+
+  const searchValue = $('.autocomplete').val(); // grab the value in the textbox here
+  // The search text which is the basis of the query
+  //  Mark the beginning of the match in a token.
+  // The upper limit the for number of suggestions to be included
+  //  Mark the end of the match in a token.
+  // in the response.  Default is set to 5.
+  const params = `?query=
+  ${encodeURIComponent(searchValue)}
+  &beginHighlight=
+  ${encodeURIComponent('<mark>')}
+  &endHighlight=
+  ${encodeURIComponent('</mark>')}
+  &maxresults=5&app_id=${APPLICATION_ID}
+  &app_code=${APPLICATION_CODE}`;
+
+  fetch(AUTOCOMPLETION_URL + params)
+    .then((response) => response.json()) // convert response to json object
+    .then((result) => {
+      const {
+        suggestions,
+      } = result;
+      createAndAppendAutoCompleteElements(suggestions);
+    })
+    .catch((error) => error);
+});
+
+// #endregion
+
+// #region map
 
 // Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  var uluru = {
-    lat: 9.08,
-    lng: 6.02
-  };
+// function initMap() {
+//   // The location of Uluru
+//   const uluru = {
+//     lat: 9.08,
+//     lng: 6.02,
+//   };
 
-  var onitsha = {
-    lat: 6.15,
-    lng: 6.79
-  };
-  var abuja = {
-    lat: 9.06,
-    lng: 7.49
-  };
+//   const onitsha = {
+//     lat: 6.15,
+//     lng: 6.79,
+//   };
+//   const abuja = {
+//     lat: 9.06,
+//     lng: 7.49,
+//   };
 
-  var icons = {
-    center: {
-      icon: "minImages/flag-red.png"
+//   const icons = {
+//     center: {
+//       icon: 'minImages/flag-red.png',
+//     },
+//     landmarks: {
+//       icon: 'minImages/flag-blue.png',
+//     },
+//   };
+//   // The map, centered at Uluru
+//   const map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 5,
+//     center: uluru,
+//   });
+//   // The marker, positioned at Uluru
+//   const centerMarker = new google.maps.Marker({
+//     position: uluru,
+//     animation: google.maps.Animation.BOUNCE,
+//     icon: icons.center.icon,
+//     map,
+//   });
+
+//   const landmark = new google.maps.Marker({
+//     position: onitsha,
+//     animation: google.maps.Animation.DROP,
+//     icon: icons.landmarks.icon,
+//     map,
+//   });
+
+//   const anotherLandmark = new google.maps.Marker({
+//     position: abuja,
+//     animation: google.maps.Animation.DROP,
+//     icon: icons.landmarks.icon,
+//     map,
+//   });
+
+//   google.maps.event.addListener(landmark, 'click', () => {
+//   });
+//   google.maps.event.addListener(anotherLandmark, 'click', () => {
+//   });
+
+//   google.maps.event.addListener(centerMarker, 'click', () => {
+//   });
+// }
+
+// #endregion
+
+function fetchGeoCodingInfoSuccessCallback(result) {
+  let geoCoordinates = {};
+
+  const {
+    Response:
+    {
+      View = 'Oops! No coordinates for this location',
+      // the array which contains geocoordinates
     },
-    landmarks: {
-      icon: "minImages/flag-blue.png"
-    }
-  };
-  // The map, centered at Uluru
-  var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 5,
-    center: uluru
-  });
-  // The marker, positioned at Uluru
-  var centerMarker = new google.maps.Marker({
-    position: uluru,
-    animation: google.maps.Animation.BOUNCE,
-    icon: icons.center.icon,
-    map: map
-  });
+  } = result;
 
-  var landmark = new google.maps.Marker({
-    position: onitsha,
-    animation: google.maps.Animation.DROP,
-    icon: icons.landmarks.icon,
-    map: map
+  View.forEach((view) => {
+    const {
+      Result = 'Oops! No coordinates for this location',
+      // an array containing the geocoordinate
+    } = view;
+    Result.forEach((res) => {
+      const {
+        Location: {
+          NavigationPosition = 'Oops! No coordinates for this location',
+          // the geocoordinates object
+        },
+      } = res;
+      geoCoordinates = NavigationPosition;
+      // insert the geocoordinates to the geoCoordinates object
+    });
   });
-
-  var anotherLandmark = new google.maps.Marker({
-    position: abuja,
-    animation: google.maps.Animation.DROP,
-    icon: icons.landmarks.icon,
-    map: map
-  });
-
-  google.maps.event.addListener(landmark, "click", function() {
-    console.log("Landmark marker clicked!");
-  });
-  google.maps.event.addListener(anotherLandmark, "click", function() {
-    console.log("anotherLandmark marker clicked!");
-  });
-
-  google.maps.event.addListener(centerMarker, "click", function() {
-    console.log("center marker clicked!");
-  });
+  return geoCoordinates;
 }
 
-//#endregion
+function getGeoCodingInfoOfSearchedPlace(searchString) {
+  if (!searchString) {
+    return 'Invalid value passed as parameter!';
+  }
+
+  const GEOCODER_URL = 'https://geocoder.api.here.com/6.2/geocode.json';
+  const urlEncodedSearchString = searchString.replace(/ /g, '+');
+  const params = `?app_id=${APPLICATION_ID}&app_code=${APPLICATION_CODE}&searchtext=${urlEncodedSearchString}`;
+
+  return fetch(GEOCODER_URL + params)
+    .then((response) => response.json()) // convert response to json object
+    .then((response) => fetchGeoCodingInfoSuccessCallback(response))
+    .catch((error) => error);
+}
+
+// function getWeatherInfoOfSearchedPlace(geoCoordinates) {
+//   const { lat, lon } = geoCoordinates;
+
+//   const OPENWEATHERMAP_URL = 'http://api.openweathermap.org/data/2.5/weather';
+//   // My application code
+//   // Mark the end of the match in a token.
+//   const params = `?lat=${lat}&lon=${lon}&appid=${OPENWEATHERMAP_APPID}`;
+
+//   return fetch(OPENWEATHERMAP_URL + params)
+//     .then((response) => response.json()) // convert response to json object
+//     .then((result) => {
+//       const { view } = result;
+//       return view;
+//     })
+//     .catch((error) => error);
+// }
+
+// function getLandmarksAroundSearchPlace(geoCoordinates) {
+//   const { lat, lon } = geoCoordinates;
+//   const GEOCODER_LANDMARKS_URL = 'https://reverse.geocoder.api.here.com/6.2/reversegeocode.json';
+
+//   // Mark the end of the match in a token.
+// const params = `?app_id=${APPLICATION_ID}&app_code=${APPLICATION_CODE}
+// & mode=retrieveLandmarks & prox=${ lon }, ${ lat }, 1000`;
+
+
+//   return fetch(GEOCODER_LANDMARKS_URL + params)
+//     .then((response) => response.json()) // convert response to json object
+//     .then((result) => {
+//       const { view } = result;
+//       return view;
+//     })
+//     .catch((error) => error);
+// }
+$(document).ready(() => {
+  // getLandmarksAroundSearchPlace(geoCoordinates);
+  // getWeatherInfoOfSearchedPlace(geoCoordinates);
+  // initMap();
+  const geoCoordinates = getGeoCodingInfoOfSearchedPlace('Port-Hacourt Nigeria');
+  geoCoordinates.then((params) => {
+    console.log(params);
+  });
+});
