cc231cc4e1768782c29d8dd594eced7496e4d018
kent2ckymaduka@gmail.com <kent2ckymaduka@gmail.com>
2019-11-20 10:42:01 +0100

    chore(fetch-apis):Recommit fetch apis
    Due to unexpected power outages, it looks like one of the local branches
    where the fetch apis was worked on was lost. Luckily the work was not lost
    so recommit the changes under a newly created branch.
    
    Changes to recommit includes the following functions:
    - getGeoCodingInfoOfSearchedPlace()
    - fetchWeatherInfoOfSearchedPlace()
    - getLandmarksAroundSearchedPlace()
    
    [Start #3]

--- gulpfile.js
+++ gulpfile.js
@@ -5,93 +5,122 @@ const terser = require('gulp-terser');
 // const uglify = require('gulp-uglify');
 const browserSync = require('browser-sync').create();
 const imageMin = require('gulp-imagemin')
+const eslint = require('gulp-eslint');
+const gulpIf = require('gulp-if');
 
 gulp.task('processHTML', gulp.series((done) => {
-	gulp.src('*.html')
-		.pipe(gulp.dest('dist'));
-	done();
+  gulp.src('*.html')
+    .pipe(gulp.dest('dist'));
+  done();
 }));
 
 gulp.task('processCSS', gulp.series((done) => {
-	gulp.src('css/*.css')
-		.pipe(gulp.dest('dist'));
-	done();
+  gulp.src('css/*.css')
+    .pipe(gulp.dest('dist'));
+  done();
 }));
 
 
 gulp.task('processJS', gulp.series((done) => {
 
-	gulp.src('js/*.js')
-		.pipe(jshint({
-			esversion: 6
-		}))
-		.pipe(jshint.reporter('default'))
-		// .pipe(babel({
-		// 	presets: ['env']
-		// }))
-		// .pipe(terser())
-		.pipe(gulp.dest('dist'));
+  gulp.src('js/*.js')
+    // .pipe(jshint({
+    // 	esversion: 6
+    // }))
+    // .pipe(jshint.reporter('default'))
+    .pipe(eslint())
+    .pipe(eslint.format())
+    // .pipe(babel({
+    // 	presets: ['env']
+    // }))
+    // .pipe(terser())
+    .pipe(gulp.dest('dist'));
 
-	done();
+  done();
 }));
 
 
+gulp.task('eslint', () => {
+  return gulp.src('js/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+});
+
+// new function added to check if ESLint has run the fix
+function isFixed(file) {
+  return file.eslint !== null && file.eslint.fixed;
+}
+
+// new lint and fix task
+gulp.task('eslint-fix', () => {
+  return gulp.src('js/*.js')
+    .pipe(eslint({
+      fix: true,
+    }))
+    .pipe(eslint.format())
+    // if running fix - replace existing file with fixed one
+    .pipe(gulpIf(isFixed, gulp.dest('dist')))
+    .pipe(eslint.failAfterError())
+});
+
+
 gulp.task('imageSquash', gulp.series((done) => {
-	gulp.src("img/*")
-		.pipe(imageMin())
-		.pipe(gulp.dest('dist/minImages'));
-	done()
+  gulp.src("img/*")
+    .pipe(imageMin())
+    .pipe(gulp.dest('dist/minImages'));
+  done()
 }));
 
 
 gulp.task('babelPolyfill', gulp.series((done) => {
-	gulp.src('node_modules/babel-polyfill/browser.js')
-		.pipe(gulp.dest('dist/node_modules/babel-polyfill'));
-	done();
+  gulp.src('node_modules/babel-polyfill/browser.js')
+    .pipe(gulp.dest('dist/node_modules/babel-polyfill'));
+  done();
 }));
 
 gulp.task('browserSync', () => {
-	browserSync.init({
-		server: './dist',
-		port: 8080,
-		ui: {
-			port: 8081
-		}
-	});
+  browserSync.init({
+    server: './dist',
+    port: 8080,
+    ui: {
+      port: 8081
+    }
+  });
 });
 
 gulp.task('watchJS', (done) => {
-	gulp.watch('js/*.js', gulp.series('processJS'));
-	done();
+  gulp.watch('js/*.js', gulp.series('processJS'));
+  done();
 });
 
 gulp.task('watchCSS', (done) => {
-	gulp.watch('css/*.css', gulp.series('processCSS'));
-	done();
+  gulp.watch('css/*.css', gulp.series('processCSS'));
+  done();
 });
 
 gulp.task('watchHTML', (done) => {
-	gulp.watch('*.html', gulp.series('processHTML'));
-	done();
+  gulp.watch('*.html', gulp.series('processHTML'));
+  done();
 });
 gulp.task('watchProdJS', (done) => {
-	gulp.watch('dist/*.js').on("change", browserSync.reload);
-	done();
+  gulp.watch('dist/*.js').on("change", browserSync.reload);
+  done();
 });
 
 gulp.task('watchProdHTML', (done) => {
-	gulp.watch('dist/*.html').on("change", browserSync.reload);
-	done();
+  gulp.watch('dist/*.html').on("change", browserSync.reload);
+  done();
 });
 
 gulp.task('watchProdCSS', (done) => {
-	gulp.watch('dist/*.css').on("change", browserSync.reload);
-	done();
+  gulp.watch('dist/*.css').on("change", browserSync.reload);
+  done();
 });
 
 
 gulp.task('watch', gulp.parallel('browserSync', 'watchJS', 'watchHTML', 'watchCSS', 'watchProdJS', 'watchProdHTML', 'watchProdCSS'));
 
 gulp.task('default', gulp.series(
-	'processHTML', 'processJS', 'processCSS', 'babelPolyfill', 'imageSquash', 'watch'
+  'processHTML', 'processJS', 'processCSS', 'babelPolyfill', 'imageSquash', 'watch'
 ));
--- js/main.js
+++ js/main.js
@@ -1,209 +1,344 @@
-const APPLICATION_ID = localStorage.getItem("_appId");
-const APPLICATION_CODE = localStorage.getItem("_appCode");
-
-$(document).ready(() => {
-  console.log("We are live!");
-});
-
-// this handles the navigation toggle functionality.
-$(".nav_handle-container").click(() => {
-  if ($(".toggled").is(":visible")) {
-    $(".toggled").hide(200);
-    return;
-  }
-  $(".toggled").show(200);
-});
-
-//#region auto suggestion
-let AUTOCOMPLETION_URL =
-  "https://autocomplete.geocoder.api.here.com/6.2/suggest.json";
-// click and keyboard events for autosuggestion.
-
-$(document).on("click", "#1", () => {
-  const suggestion = $("#1").text();
-  insertSuggestionToTextbox(suggestion);
-});
-
-$(document).on("click", "#2", () => {
-  const suggestion = $("#2").text();
-  insertSuggestionToTextbox(suggestion);
-});
-
-$(document).on("click", "#3", () => {
-  const suggestion = $("#3").text();
-  insertSuggestionToTextbox(suggestion);
-});
-
-$(document).on("click", "#4", () => {
-  const suggestion = $("#4").text();
-  insertSuggestionToTextbox(suggestion);
-});
-
-$(document).on("click", "#5", () => {
-  const suggestion = $("#5").text();
-  insertSuggestionToTextbox(suggestion);
-});
-
-$(document).on("keyup", "#1", e => {
-  if (e.which == 13 || e.which == 32) {
-    const suggestion = $("#1").text();
-    insertSuggestionToTextbox(suggestion);
-  }
-});
-
-$(document).on("keyup", "#2", e => {
-  if (e.which == 13 || e.which == 32) {
-    const suggestion = $("#2").text();
-    insertSuggestionToTextbox(suggestion);
-  }
-});
-
-$(document).on("keyup", "#3", e => {
-  if (e.which == 13 || e.which == 32) {
-    const suggestion = $("#3").text();
-    insertSuggestionToTextbox(suggestion);
-  }
-});
-
-$(document).on("keyup", "#4", e => {
-  if (e.which == 13 || e.which == 32) {
-    const suggestion = $("#4").text();
-    insertSuggestionToTextbox(suggestion);
-  }
-});
-
-$(document).on("keyup", "#5", e => {
-  if (e.which == 13 || e.which == 32) {
-    const suggestion = $("#5").text();
-    insertSuggestionToTextbox(suggestion);
-  }
-});
+const APPLICATION_ID = localStorage.getItem('_appId');
+const APPLICATION_CODE = localStorage.getItem('_appCode');
+const OPENWEATHERMAP_APPID = localStorage.getItem('_openWeatherMapAppId');
 
 function insertSuggestionToTextbox(suggestion) {
-  $(".suggestions").remove();
-  $("input").val(suggestion);
-  $("input").focus();
+  $('.suggestions').remove();
+  $('input').val(suggestion);
+  $('input').focus();
 }
 
 function createAndAppendAutoCompleteElements(suggestions) {
-  // early exit
-  if (!suggestions) {
-    $(".suggestions").remove();
-    console.log("suggestions is: " + suggestions);
-    return;
-  }
-
-  $(".suggestions").remove();
-  console.log(suggestions);
+  $('.suggestions').remove();
   let id = 0;
-  let suggestion = suggestions.map(suggestions => {
-    id++;
-    return `<div class="suggestions" id=${id} tabindex="0"> ${suggestions.label} </div>`;
+  const result = suggestions.map((suggestion) => {
+    id += 1;
+    return `<div class="suggestions" id=${id} tabindex="0"> ${suggestion.label} </div>`;
   });
-  $("form").append(suggestion);
+  $('form').append(result);
 }
 
-$(".autocomplete").keyup(e => {
-  // Exit on esc keypress event
-  if (e.which == 27) {
-    $(".suggestions").remove();
+
+// this handles the navigation toggle functionality.
+$('.nav_handle-container').click(() => {
+  if ($('.toggled').is(':visible')) {
+    $('.toggled').hide(200);
     return;
   }
-
-  let searchValue = $(".autocomplete").val(); //grab the value in the textbox here
-
-  let params =
-    "?" +
-    "query=" +
-    encodeURIComponent(searchValue) + // The search text which is the basis of the query
-    "&beginHighlight=" +
-    encodeURIComponent("<mark>") + //  Mark the beginning of the match in a token.
-    "&endHighlight=" +
-    encodeURIComponent("</mark>") + //  Mark the end of the match in a token.
-    "&maxresults=5" + // The upper limit the for number of suggestions to be included
-    // in the response.  Default is set to 5.
-    "&app_id=" +
-    APPLICATION_ID +
-    "&app_code=" +
-    APPLICATION_CODE;
-
-  fetch(AUTOCOMPLETION_URL + params)
-    .then(response => response.json()) // convert response to json object
-    .then(result => {
-      const { suggestions } = result;
-      createAndAppendAutoCompleteElements(suggestions);
-    })
-    .catch(error => {
-      console.log("error fetching autocompletion data.");
-    });
+  $('.toggled').show(200);
 });
 
-//#endregion
+// #region auto suggestion
+const AUTOCOMPLETION_URL = 'https://autocomplete.geocoder.api.here.com/6.2/suggest.json';
+// click and keyboard events for autosuggestion.
 
-//#region map
+$(document).on('click', '#1', () => {
+  const suggestion = $('#1').text();
+  insertSuggestionToTextbox(suggestion);
+});
+
+$(document).on('click', '#2', () => {
+  const suggestion = $('#2').text();
+  insertSuggestionToTextbox(suggestion);
+});
+
+$(document).on('click', '#3', () => {
+  const suggestion = $('#3').text();
+  insertSuggestionToTextbox(suggestion);
+});
+
+$(document).on('click', '#4', () => {
+  const suggestion = $('#4').text();
+  insertSuggestionToTextbox(suggestion);
+});
+
+$(document).on('click', '#5', () => {
+  const suggestion = $('#5').text();
+  insertSuggestionToTextbox(suggestion);
+});
+
+$(document).on('keyup', '#1', (e) => {
+  if (e.which === 13 || e.which === 32) {
+    const suggestion = $('#1').text();
+    insertSuggestionToTextbox(suggestion);
+  }
+});
+
+$(document).on('keyup', '#2', (e) => {
+  if (e.which === 13 || e.which === 32) {
+    const suggestion = $('#2').text();
+    insertSuggestionToTextbox(suggestion);
+  }
+});
+
+$(document).on('keyup', '#3', (e) => {
+  if (e.which === 13 || e.which === 32) {
+    const suggestion = $('#3').text();
+    insertSuggestionToTextbox(suggestion);
+  }
+});
+
+$(document).on('keyup', '#4', (e) => {
+  if (e.which === 13 || e.which === 32) {
+    const suggestion = $('#4').text();
+    insertSuggestionToTextbox(suggestion);
+  }
+});
+
+$(document).on('keyup', '#5', (e) => {
+  if (e.which === 13 || e.which === 32) {
+    const suggestion = $('#5').text();
+    insertSuggestionToTextbox(suggestion);
+  }
+});
+
+$('.autocomplete').keyup((e) => {
+  // Exit on esc keypress event
+  if (e.which === 27) {
+    $('.suggestions').remove();
+    return;
+  }
+
+  const searchValue = $('.autocomplete').val(); // grab the value in the textbox here
+  // The search text which is the basis of the query
+  //  Mark the beginning of the match in a token.
+  // The upper limit the for number of suggestions to be included
+  //  Mark the end of the match in a token.
+  // in the response.  Default is set to 5.
+  const params = `?query=
+  ${encodeURIComponent(searchValue)}
+  &beginHighlight=
+  ${encodeURIComponent('<mark>')}
+  &endHighlight=
+  ${encodeURIComponent('</mark>')}
+  &maxresults=5&app_id=${APPLICATION_ID}
+  &app_code=${APPLICATION_CODE}`;
+
+  fetch(AUTOCOMPLETION_URL + params)
+    .then((response) => response.json()) // convert response to json object
+    .then((result) => {
+      const {
+        suggestions,
+      } = result;
+      createAndAppendAutoCompleteElements(suggestions);
+    })
+    .catch((error) => error);
+});
+
+// #endregion
+
+// #region map
 
 // Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  var uluru = {
-    lat: 9.08,
-    lng: 6.02
-  };
+// function initMap() {
+//   // The location of Uluru
+//   const uluru = {
+//     lat: 9.08,
+//     lng: 6.02,
+//   };
 
-  var onitsha = {
-    lat: 6.15,
-    lng: 6.79
-  };
-  var abuja = {
-    lat: 9.06,
-    lng: 7.49
-  };
+//   const onitsha = {
+//     lat: 6.15,
+//     lng: 6.79,
+//   };
+//   const abuja = {
+//     lat: 9.06,
+//     lng: 7.49,
+//   };
 
-  var icons = {
-    center: {
-      icon: "minImages/flag-red.png"
-    },
-    landmarks: {
-      icon: "minImages/flag-blue.png"
-    }
-  };
-  // The map, centered at Uluru
-  var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 5,
-    center: uluru
-  });
-  // The marker, positioned at Uluru
-  var centerMarker = new google.maps.Marker({
-    position: uluru,
-    animation: google.maps.Animation.BOUNCE,
-    icon: icons.center.icon,
-    map: map
-  });
+//   const icons = {
+//     center: {
+//       icon: 'minImages/flag-red.png',
+//     },
+//     landmarks: {
+//       icon: 'minImages/flag-blue.png',
+//     },
+//   };
+//   // The map, centered at Uluru
+//   const map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 5,
+//     center: uluru,
+//   });
+//   // The marker, positioned at Uluru
+//   const centerMarker = new google.maps.Marker({
+//     position: uluru,
+//     animation: google.maps.Animation.BOUNCE,
+//     icon: icons.center.icon,
+//     map,
+//   });
 
-  var landmark = new google.maps.Marker({
-    position: onitsha,
-    animation: google.maps.Animation.DROP,
-    icon: icons.landmarks.icon,
-    map: map
-  });
+//   const landmark = new google.maps.Marker({
+//     position: onitsha,
+//     animation: google.maps.Animation.DROP,
+//     icon: icons.landmarks.icon,
+//     map,
+//   });
 
-  var anotherLandmark = new google.maps.Marker({
-    position: abuja,
-    animation: google.maps.Animation.DROP,
-    icon: icons.landmarks.icon,
-    map: map
-  });
+//   const anotherLandmark = new google.maps.Marker({
+//     position: abuja,
+//     animation: google.maps.Animation.DROP,
+//     icon: icons.landmarks.icon,
+//     map,
+//   });
 
-  google.maps.event.addListener(landmark, "click", function() {
-    console.log("Landmark marker clicked!");
-  });
-  google.maps.event.addListener(anotherLandmark, "click", function() {
-    console.log("anotherLandmark marker clicked!");
-  });
+//   google.maps.event.addListener(landmark, 'click', () => {
+//   });
+//   google.maps.event.addListener(anotherLandmark, 'click', () => {
+//   });
 
-  google.maps.event.addListener(centerMarker, "click", function() {
-    console.log("center marker clicked!");
+//   google.maps.event.addListener(centerMarker, 'click', () => {
+//   });
+// }
+
+// #endregion
+
+// #region fetch APIs
+
+function fetchGeoCodingInfoSuccessCallback(result) {
+  if (!result || result.type === 'ApplicationError') {
+    throw new Error(`Error fetching geocoding information: ${result.type} `);
+  }
+  const geoCoordinates = {};
+  const {
+    Result = 'Oops! No coordinates for this location',
+    // the array which contains geocoordinates
+  } = result.Response.View[0];
+
+  Result.forEach((res) => {
+    const {
+      Latitude = 'Oops! No coordinates for this location',
+      // an array containing the geocoordinate,
+      Longitude = 'Oops! No coordinates for this location',
+
+    } = res.Location.NavigationPosition[0];
+    geoCoordinates.Latitude = Latitude;
+    geoCoordinates.Longitude = Longitude;
+    // insert the geocoordinates to the geoCoordinates object
   });
+  return geoCoordinates;
 }
 
-//#endregion
+function getGeoCodingInfoOfSearchedPlace(searchString) {
+  if (!searchString) {
+    return 'Invalid value passed as parameter!';
+  }
+
+  const GEOCODER_URL = 'https://geocoder.api.here.com/6.2/geocode.json';
+  const urlEncodedSearchString = searchString.replace(/ /g, '+');
+  const params = `?app_id=${APPLICATION_ID}&app_code=${APPLICATION_CODE}&searchtext=${urlEncodedSearchString}`;
+  return fetch(GEOCODER_URL + params)
+    .then((response) => response.json()) // convert response to json object
+    .then((response) => fetchGeoCodingInfoSuccessCallback(response))
+    .catch((error) => error);
+}
+
+function fetchWeatherInfoOfSearchedPlaceSuccessCallback(result) {
+  if (result.cod !== 200) {
+    throw new Error(`Error fetching geocoding information: ${result.type} `);
+  }
+  const weatherInfo = {};
+  const {
+    coord = {},
+    weather = [],
+    main = {},
+    wind = {},
+  } = result; // get only weather, main and wind from returned json;
+  weatherInfo.geoCoordinates = coord;
+
+  weather.forEach((obj) => {
+    const {
+      main: currentWeather = 'Main weather not found.',
+      // get only main from obj in the weather array and rename it 'currentWeather'
+    } = obj;
+    weatherInfo.currentWeather = currentWeather; // insert currentWeather into weatherInfo obj
+  });
+  const {
+    humidity = 'Humidity not found.',
+    temp: temperature = 'Temperature not found.',
+  } = main; // select only humidity and temp (renamed 'temperature') from the 'main' object
+  weatherInfo.humidity = humidity;
+  weatherInfo.temperature = temperature;
+  // insert them into the weatherInfo object
+
+  const {
+    speed: windSpeed = 'Wind speed not found.',
+  } = wind; // select only speed (renamed windSpeed)from the wind obj
+  weatherInfo.windSpeed = windSpeed; // insert windSpeed into weatherInfo obj
+  return weatherInfo;
+}
+
+function getWeatherInfoOfSearchedPlace(lat, lon) {
+  const OPENWEATHERMAP_URL = 'http://api.openweathermap.org/data/2.5/weather';
+  const params = `?lat=${lat}&lon=${lon}&appid=${OPENWEATHERMAP_APPID}`;
+  return fetch(OPENWEATHERMAP_URL + params)
+    .then((response) => response.json()) // convert response to json object
+    .then((result) => fetchWeatherInfoOfSearchedPlaceSuccessCallback(result))
+    .catch((error) => error);
+}
+
+function getLandmarksAroundSearchedPlaceSuccessCallBack(result) {
+  if (!result || result.type === 'ApplicationError') {
+    throw new Error(`Error fetching geocoding information: ${result.type} `);
+  }
+  const landmark = {};
+  const landmarks = [];
+  const { Result } = result.Response.View[0];
+  Result.forEach((obj) => {
+    const {
+      Location: {
+        LocationType = 'Nothing here.',
+        Name = 'Nothing here',
+        DisplayPosition = 'Nothing here.',
+        Address: {
+          Label = 'Nothing here.',
+          Country = 'Nothing here',
+          State = 'Nothing here',
+          City = 'Nothing here',
+        },
+      },
+    } = obj;
+
+    landmark.locationType = LocationType;
+    landmark.name = Name;
+    landmark.displayPosition = DisplayPosition;
+    landmark.label = Label;
+    landmark.country = Country;
+    landmark.state = State;
+    landmark.city = City;
+
+    landmarks.push(landmark);
+  });
+
+  return landmarks;
+}
+
+function getLandmarksAroundSearchedPlace(geoCoordinates) {
+  if (!geoCoordinates) {
+    throw new Error(`Error fetching geocoding information: ${geoCoordinates} `);
+  }
+  const {
+    lat = '',
+    lon = '',
+  } = geoCoordinates;
+  const GEOCODER_LANDMARKS_URL = 'https://reverse.geocoder.api.here.com/6.2/reversegeocode.json';
+  // Mark the end of the match in a token.
+  const params = `?app_id=${APPLICATION_ID}&app_code=${APPLICATION_CODE}&mode=retrieveLandmarks&prox=${lat},${lon},1000`;
+  return fetch(GEOCODER_LANDMARKS_URL + params)
+    .then((response) => response.json()) // convert response to json object
+    .then((result) => getLandmarksAroundSearchedPlaceSuccessCallBack(result))
+    .catch((error) => error);
+}
+
+// #endregion
+
+$(document).ready(() => {
+  // initMap();
+  const geoCoordinatesPromise = getGeoCodingInfoOfSearchedPlace(' Nigeria,     Lagos    ,     Lagos    ,     Mile     12 Brg ');
+  geoCoordinatesPromise.then((params) => {
+    const { Latitude, Longitude } = params;
+    return getWeatherInfoOfSearchedPlace(Latitude, Longitude);
+  })
+    .then((res) => getLandmarksAroundSearchedPlace(res.geoCoordinates))
+    .then((result) => console.log(result))
+    .catch((Error) => console.log(`This is from catch: ${Error} `));
+});
